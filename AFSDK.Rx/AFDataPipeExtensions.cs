4128968b7791beb9121b41e487e4a1aadddfd8c1
barry.shang <barry.shang@gmail.com>
2015-09-28 14:42:36 -0700

    Added code comments.

--- AFSDK.Rx/AFDataPipeExtensions.cs
+++ AFSDK.Rx/AFDataPipeExtensions.cs
@@ -10,6 +10,19 @@ namespace AFSDK.Rx
 {
     public static class AFDataPipeExtensions
     {
+        /// <summary>
+        /// Extension method on AFDataPipe to create an IObservable. A subscription to the underlying AFDataPipe is made.
+        /// An EventLoopScheduler is set up to poll GetObserverEvents at a configurable interval.
+        /// The Publish/RefCount construct is used to handle subscription side-effects and share the underlying AFDataPipe subscription
+        /// with mulitple observers.
+        /// See http://www.introtorx.com/content/v1.0.10621.0/14_HotAndColdObservables.html for Publish/RefCount documentation.
+        /// See http://davesexton.com/blog/post/To-Use-Subject-Or-Not-To-Use-Subject.aspx for the Publish/RefCount use case.
+        /// See http://stackoverflow.com/questions/14396449/why-are-subjects-not-recommended-in-net-reactive-extensions for EventLoopScheduler and recursion idea.
+        /// See http://www.zerobugbuild.com/?p=259 for recursive scheduling idea.
+        /// </summary>
+        /// <param name="dataPipe"></param>
+        /// <param name="seconds">Number of seconds to wait before calling GetObserverEvents</param>
+        /// <returns></returns>
         public static IObservable<AFDataPipeEvent> CreateObservable(this AFDataPipe dataPipe, double seconds = 1)
         {
             TimeSpan interval = TimeSpan.FromSeconds(seconds);
